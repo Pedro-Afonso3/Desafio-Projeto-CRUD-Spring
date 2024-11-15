9f75653cea5999f0dadd85209d24bb0e440a7475
Pedro-Afonso3 <paccj3@gmail.com>
2024-11-15 12:52:05 -0300

    Primeiro Commit

---
+++ src/main/java/one/dio/Desafio_projetos_CRUD/DesafioProjetosCrudApplication.java
@@ -0,0 +1,13 @@
+package one.dio.Desafio_projetos_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProjetosCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProjetosCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/dio/Desafio_projetos_CRUD/DesafioProjetosCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package one.dio.Desafio_projetos_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProjetosCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
