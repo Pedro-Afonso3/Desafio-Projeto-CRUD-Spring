e6bc7b692bedbf7bc8e77135fa408cafede393c9
Pedro-Afonso3 <paccj3@gmail.com>
2024-11-15 18:53:30 -0300

    Last Commit

--- src/main/java/one/dio/Desafio_projetos_CRUD/controller/UsuarioController.java
+++ src/main/java/one/dio/Desafio_projetos_CRUD/controller/UsuarioController.java
@@ -7,8 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("usuar" +
-        "ios")
+@RequestMapping("usuarios")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
